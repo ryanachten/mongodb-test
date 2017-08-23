a31cb7122e056fcdfbfd4155c6d7de57e7c38098
Ryan Achten <ryanachten@gmail.com>
2017-08-23 14:09:37 +1200

    Add initial http api using express and body-parser

--- server/server.js
+++ server/server.js
@@ -1,53 +1,28 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
+var {Todo} = require('./models/todo');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(
+    (doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.send(e);
+    });
 });
 
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
+app.listen(3000, ()=>{
+  console.log('Started on port 3000');
 });
-
-// var newTodo = new Todo({
-//   text: 'Learn node'
-// });
-//
-// newTodo.save().then((doc)=>{
-//   console.log('Saved todo', doc);
-// },(e) =>{
-//   console.log(e);
-// });
-
-var newUser = new User({
-  // email: 'ryanachten@gmail.com'
-});
-
-newUser.save().then(
-  (doc)=>{
-    console.log('Save doc', doc);
-  },
-  (e)=>{
-    console.log(e);
-  }
-);
