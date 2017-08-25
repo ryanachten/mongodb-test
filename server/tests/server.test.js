e1b53961c05ca6e070859cecfc90f3c0de0c6943
Ryan Achten <ryanachten@gmail.com>
2017-08-26 11:17:57 +1200

    Test DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -87,10 +87,46 @@ describe('GET /todos/:id', ()=>{
       .end(done);
   })
 
-  it('should return 404 for non object ids', (done)=>{
+  it('should return 404 for invalid object ids', (done)=>{
     request(app)
       .get(`/todos/123`)
       .expect(404)
       .end(done);
   })
+});
+
+describe('DELETE /todos/:id', ()=>{
+
+  it('should remove a todo', (done)=>{
+
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if(err) return done(err);
+        Todo.findById(hexId).then((todo)=>{
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found doc', (done)=>{
+    var tempId = new ObjectId().toHexString();
+    request(app)
+      .delete(`/todos/${tempId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for invalid object ids', (done)=>{
+    request(app)
+      .delete(`/todos/123`)
+      .expect(404)
+      .end(done);
+  });
 });
